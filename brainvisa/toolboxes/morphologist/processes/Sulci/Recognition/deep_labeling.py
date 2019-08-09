95a7414523e7faa3f796cc374287c94062922408
Denis Rivière <denis.riviere.gm@gmail.com>
2019-08-09 11:27:49 +0200

    added validation in deepsulci labeling process

--- brainvisa/toolboxes/morphologist/processes/Sulci/Recognition/deep_labeling.py
+++ brainvisa/toolboxes/morphologist/processes/Sulci/Recognition/deep_labeling.py
@@ -8,11 +8,28 @@ Created on Wed Jul 24 11:06:07 2019
 from brainvisa.processes import *
 from brainvisa.processing import capsul_process
 
-name = 'Deep labeling'
+name = 'Sulci recognition with Deep CNN'
 userLevel = 0
 
 base_class = capsul_process.CapsulProcess
 capsul_process = 'deepsulci.sulci_labeling.capsul.labeling'
+
+def validation():
+    try:
+        import torch
+    except ImportError:
+        raise ValidationError('PyTorch (torch module) needs to be installed '
+                              'to have CNN sulci recognition working')
+    shared_db = [db for db in neuroConfig.dataPath
+                  if db.expert_settings.ontology == 'shared']
+    models = [os.path.exists(p)
+              for p in
+                  [os.path.join(db.directory, 'models', 'models_2019',
+                                'cnn_models', 'sulci_unet_model_left.mdsm')
+                   for db in shared_db]]
+    if len(models) == 0:
+        raise ValidationError('CNN sulci models are not installed')
+
 
 signature = Signature(
     'roots', ReadDiskItem('Cortex catchment bassins',
