9e20c3e90344c1612bbe6bded521b374ebcc63d3
Denis Rivière <denis.riviere.gm@gmail.com>
2020-04-08 14:45:56 +0200

    fix in doc config

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -321,7 +321,7 @@ intersphinx_mapping = {
               None),
     'somaworkflow': (os.path.join(docpath, 'soma-workflow-'
                                   + somaworkflow_version + '/sphinx'), None),
-    'python': ('http://docs.python.org/2.7', None),
+    'python': ('http://docs.python.org/%d.%d' % sys.version_info[:2], None),
 }
 
 # generate pipeline and processes docs
