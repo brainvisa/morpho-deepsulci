25a0942899c07c9ef24e74f9e56192e0c7913a87
Denis Rivière <denis.riviere.gm@gmail.com>
2023-08-31 09:55:47 +0200

    fetch data from brainvisa.info, not ftp.cea.fr
    (brainvisa/brainvisa.github.io#149)

--- scripts/bv_get_morpho_deepsulci_models.py
+++ scripts/bv_get_morpho_deepsulci_models.py
@@ -33,7 +33,7 @@ class Context(object):
         os.close(tfile[0])
         return filename
 
-download_url = 'ftp://ftp.cea.fr/pub/dsv/anatomist/data'
+download_url = 'https://brainvisa.info/download/data'
 modelversion = '2019'
 
 parser = OptionParser(
@@ -45,7 +45,7 @@ parser.add_option('-i', '--input',
 parser.add_option('-v', '--version',
                   help='model version, default: %s' % modelversion)
 parser.add_option('-t', '--timeout', type='float',
-                  help='ftp timeout (in seconds), default: 15', default=15)
+                  help='timeout (in seconds), default: 15', default=15)
 parser.add_option('-s', '--silent', action='store_true',
                   help='do not raise an error when the timeout fails')
 parser.add_option('--check', action='store_true',
@@ -79,7 +79,8 @@ if not os.path.exists(destdir):
     os.makedirs(destdir)
 context.write('install in dir:', destdir)
 
-if download_url.startswith('ftp://'):
+if download_url.startswith('ftp://') or download_url.startswith('https://') \
+        or download_url.startswith('http://'):
     local_files = False
 else:
     local_files = True
